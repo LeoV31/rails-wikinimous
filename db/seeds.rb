11ef35a1bc415c1ab5adb5684aa6ab517a9081cb
Leo Viguier <leo.viguier@essec.edu>
2024-02-20 16:26:08 +0100

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+  article = Article.new
+  article.title = Faker::Book.title
+  article.content = Faker::Lorem.paragraph
+  article.save
+end
